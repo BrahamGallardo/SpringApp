800699a181fe13c3c151e216711be57e1190b735
BrahamGallardo <braham.gc@gmail.com>
2021-06-09 03:38:13 -0500

    Repositories and services created, update views

--- spring_app/src/main/java/com/example/demo/controller/UserController.java
+++ spring_app/src/main/java/com/example/demo/controller/UserController.java
@@ -1,18 +1,36 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.RoleRepository;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
+
 
 @Controller
 public class UserController {
+
+	@Autowired
+	RoleRepository roleRepository;
 	
-	@GetMapping("/")
-	public String index() {
-		return "index";
-	}
+	@Autowired 
+	UserService userService;
 	
 	@GetMapping("/userForm")
-	public String getUserForm() {
+	public String getUserForm(Model model) {
+		model.addAttribute("userForm", new User());
+		model.addAttribute("roles",roleRepository.findAll());
+		model.addAttribute("userList", userService.getAllUsers());
+		model.addAttribute("listTab","active");
 		return "user-form/user-view";
+	}
+	
+	@GetMapping({"/","/login"})
+	public String index() {
+		return "index";
 	}
 }
