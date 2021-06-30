12c310725e0ede80692dc303da168315113267ed
BrahamGallardo <braham.gc@gmail.com>
2021-06-29 23:46:25 -0500

    Update user already

--- spring_app/src/main/java/com/example/demo/service/UserService.java
+++ spring_app/src/main/java/com/example/demo/service/UserService.java
@@ -7,5 +7,7 @@ public interface UserService {
 	public Iterable<User> getAllUsers();
 	
 	public User createUser(User formUser) throws Exception;
+	public User getUserById(Long id) throws Exception;
+	public User updateUser(User user) throws Exception;
 		
 }
--- spring_app/src/main/java/com/example/demo/service/UserServiceImpl.java
+++ spring_app/src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -8,15 +8,15 @@ import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
 	@Autowired
 	UserRepository userRepository;
-	
-	public Iterable<User> getAllUsers(){
+
+	public Iterable<User> getAllUsers() {
 		return userRepository.findAll();
 	}
-	
+
 	private boolean checkUsernameAvailable(User user) throws Exception {
 		Optional<User> userFound = userRepository.findByUsername(user.getUsername());
 		if (userFound.isPresent()) {
@@ -26,12 +26,15 @@ public class UserServiceImpl implements UserService{
 	}
 
 	private boolean checkPasswordValid(User user) throws Exception {
-		if ( !user.getPassword().equals(user.getConfirmPassword())) {
+		if (user.getConfirmPassword()==null || user.getConfirmPassword().isEmpty()) {
+			throw new Exception("Campo confirm password esta vacio");
+		}
+		
+		if (!user.getPassword().equals(user.getConfirmPassword())) {
 			throw new Exception("Password y Confirm Password no son iguales");
 		}
 		return true;
 	}
-
 
 	@Override
 	public User createUser(User user) throws Exception {
@@ -40,5 +43,32 @@ public class UserServiceImpl implements UserService{
 		}
 		return user;
 	}
-	
+
+	@Override
+	public User getUserById(Long id) throws Exception {
+		User user = userRepository.findById(id).orElseThrow(() -> new Exception("El usuario no existe"));
+		return user;
+	}
+
+	@Override
+	public User updateUser(User fromUser) throws Exception {
+		User toUser = getUserById(fromUser.getId());
+		mapUser(fromUser, toUser);
+		return userRepository.save(toUser);
+	}
+
+	/**
+	 * Map everything but the password.
+	 * 
+	 * @param from
+	 * @param to
+	 */
+	protected void mapUser(User from, User to) {
+		to.setUsername(from.getUsername());
+		to.setFirstName(from.getFirstName());
+		to.setLastName(from.getLastName());
+		to.setEmail(from.getEmail());
+		to.setRoles(from.getRoles());
+	}
+
 }
